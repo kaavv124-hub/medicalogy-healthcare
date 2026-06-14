81fcc436a60f8d08a21452976c4e56b76ad519c3
V Kaviya <kaavv124@gmail.com>
2026-06-14 18:27:17 +0530

    Added robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://medicalogy-healthcare.vercel.app/sitemap.xml',
+  }
+}
