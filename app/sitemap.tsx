fdedfe795203818b971c781eb258780b4c7a84db
V Kaviya <kaavv124@gmail.com>
2026-06-14 18:07:31 +0530

    Added sitemap

---
+++ app/sitemap.tsx
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://medicalogy-healthcare.vercel.app',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://medicalogy-healthcare.vercel.app/services',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://medicalogy-healthcare.vercel.app/internships',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://medicalogy-healthcare.vercel.app/projects',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://medicalogy-healthcare.vercel.app/gallery',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://medicalogy-healthcare.vercel.app/contact',
+      lastModified: new Date(),
+    },
+  ]
+}
